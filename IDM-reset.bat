e7d8eab5173c667acb717b839ad56fa0de0fbce4
jetfir3 <95306468+jetfir3@users.noreply.github.com>
2021-12-15 23:31:13 -0500

    Update IDM-reset.bat

--- IDM-reset.bat
+++ IDM-reset.bat
@@ -44,6 +44,8 @@ reg delete "HKEY_CURRENT_USER\Software\Classes\Wow6432Node\CLSID\{6DDF00DB-1234-
 reg delete "HKEY_CURRENT_USER\Software\Classes\Wow6432Node\CLSID\{7B8E9164-324D-4A2E-A46D-0165FB2000EC}" /f 2> nul
 reg delete "HKEY_CURRENT_USER\Software\Classes\Wow6432Node\CLSID\{5ED60779-4DE2-4E07-B862-974CA4FF2E9C}" /f 2> nul
 
+rem reg delete "HKEY_LOCAL_MACHINE\Software\Classes\Wow6432Node\CLSID\{5ED60779-4DE2-4E07-B862-974CA4FF2E9C}" /f 2> nul
+rem reg delete "HKEY_LOCAL_MACHINE\Software\Classes\Wow6432Node\CLSID\{7B8E9164-324D-4A2E-A46D-0165FB2000EC}" /f 2> nul
 reg delete "HKEY_LOCAL_MACHINE\Software\Wow6432Node\Internet Download Manager" /v scansk /f 2> nul
 reg delete "HKEY_LOCAL_MACHINE\Software\Wow6432Node\Internet DownloadManager" /v FName /f 2> nul
 reg delete "HKEY_LOCAL_MACHINE\Software\Wow6432Node\Internet DownloadManager" /v LName /f 2> nul
